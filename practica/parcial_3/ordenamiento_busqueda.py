1ce764008e8322cec51988380af77db47d0919af
FranciscoGiayetto <frangiayetto@gmail.com>
2024-09-09 18:12:04 -0300

    Refactor sorting and searching functions

--- practica/parcial_3/ordenamiento_busqueda.py
+++ practica/parcial_3/ordenamiento_busqueda.py
@@ -1,20 +1,22 @@
-lista= [2,1,4,6,536,3,4,5,6,7]
+lista = [2, 1, 4, 6, 536, 3, 4, 5, 6, 7]
+
 
 def ordenar(lista):
-    largo= len(lista)
-    medio= largo//2
-    while medio >0:
-        for i in range(medio,largo):
-            temp= lista[i]
-            j=i
-            while j>=medio and lista[j-medio]>temp:
-                lista[j]= lista[j-medio]
+    largo = len(lista)
+    medio = largo // 2
+    while medio > 0:
+        for i in range(medio, largo):
+            temp = lista[i]
+            j = i
+            while j >= medio and lista[j - medio] > temp:
+                lista[j] = lista[j - medio]
                 j -= medio
-            lista[j]= temp
+            lista[j] = temp
         medio //= 2
 
     print(lista)
     return lista
+
 
 def busqueda_binaria(lista, objetivo):
     inicio = 0
@@ -23,14 +25,14 @@ def busqueda_binaria(lista, objetivo):
     while inicio <= fin:
         medio = (inicio + fin) // 2
         if lista[medio] == objetivo:
-            return 'encontrado'
+            return "encontrado"
         elif lista[medio] < objetivo:
             inicio = medio + 1
         else:
             fin = medio - 1
 
-    return 'No enconctrado'
+    return "No enconctrado"
 
 
-lista_ordenada=ordenar(lista)
-print(busqueda_binaria(lista_ordenada,100))
+lista_ordenada = ordenar(lista)
+print(busqueda_binaria(lista_ordenada, 100))
