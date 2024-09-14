0f301fd4e412a253c19377b584bd336840068280
FranciscoGiayetto <frangiayetto@gmail.com>
2024-09-14 17:18:56 -0300

    Refactor code to display unique numbers and their frequencies

--- Desafios/desafio_3/Desafio3.py
+++ Desafios/desafio_3/Desafio3.py
@@ -1,34 +1,18 @@
 import soporte
 from collections import Counter
+def mostrar(lista,numero):
+    print('-'*20)
+    print(f'Parte {numero}')
+    print('-'*20)
+    numeros_unicos = list(set(lista))
+    print('Numeros unicos ', len(numeros_unicos))
 
-#Parte 1
-print('-'*20)
-print('Parte 1')
-print('-'*20)
-lista = soporte.vector_unknown_range(300000)
-numeros_unicos = list(set(lista))
-print('Numeros unicos ', len(numeros_unicos))
+    contador = Counter(lista)
+    max_frecuencia = max(contador.values())
 
+    numeros_mas_frecuentes = [num for num, count in contador.items() if count == max_frecuencia]
 
-contador = Counter(lista)
-max_frecuencia = max(contador.values())  # La mayor cantidad de repeticiones
+    print(f"Los números que más se repiten son {numeros_mas_frecuentes} y se repiten {max_frecuencia} veces.")
 
-numeros_mas_frecuentes = [num for num, count in contador.items() if count == max_frecuencia]
-
-print(f"Los números que más se repiten son {numeros_mas_frecuentes} y se repiten {max_frecuencia} veces.")
-
-#Parte 2
-print('-'*20)
-print('Parte 2')
-print('-'*20)
-lista = soporte.vector_known_range(300000)
-numeros_unicos = list(set(lista))
-print('Numeros unicos ', len(numeros_unicos))
-
-
-contador = Counter(lista)
-max_frecuencia = max(contador.values())  # La mayor cantidad de repeticiones
-
-numeros_mas_frecuentes = [num for num, count in contador.items() if count == max_frecuencia]
-
-print(f"Los números que más se repiten son {numeros_mas_frecuentes} y se repiten {max_frecuencia} veces.")
+mostrar(soporte.vector_unknown_range(300000),1)
+mostrar(soporte.vector_known_range(300000),1)
